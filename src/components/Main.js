bce852643784644e7c6e6207e19b470d1d206b2d
vlad05x <organizm777alf@gmail.com>
2024-07-15 08:47:57 +0300

    Сreated the main file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,30 @@
 import React from "react";
+import MainPhoto from "../images/main-photo.svg";
 
 function Main() {
   return (
     <div className="wrapper">
-        <main className="main">
-            <div className="main__container">
-                <div className="main_content">
-                    <h1 className="main_content__title">
-                        EPICSTARS — платформа, объединяющая рекламодателей и блогеров
-                    </h1>
-                    <p className="main_content__text">
-                        Помогаем брендам и компаниям транслировать свои маркетинговые активности через лидеров мнений и блогеров
-                    </p>
-                    <button className="main_btn">Давайте начнем!</button>
-                </div>
-            </div>
-        </main>
+      <main className="main">
+        <div className="main__container">
+          <div className="main_content">
+            <h1 className="main_content__title">
+              EPICSTARS — платформа, объединяющая <span style={{ textDecoration: 'underline' }}>рекламодателей</span> и <span style={{ textDecoration: 'underline' }}>блогеров</span>
+            </h1>
+            <p className="main_content__text">
+              Помогаем брендам и компаниям транслировать свои маркетинговые
+              активности через лидеров мнений и блогеров
+            </p>
+            <button className="main_btn">Давайте начнем!</button>
+          </div>
+          <img
+            className="main_photo"
+            src={MainPhoto}
+            alt="Описание изображения"
+          />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./style/App.scss";
-import "./components/Header/header.scss";
 import "./style/reset.css";
 import "./style/responsive.scss";
+import "./components/Header/header.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
